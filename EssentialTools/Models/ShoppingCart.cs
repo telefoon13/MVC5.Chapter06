3347805ea0bbcc8753bf4e273c8c5306a492019b
telefoon13 <Mike0528>
2018-03-06 17:22:09 +0100

    First Commit

---
+++ EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        public IEnumerable<Product> Products { get; set; }
+        private LinqValueCalculator calc;
+
+
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+
+    }
+}
